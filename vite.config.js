57d204be85f4951e080c0bc71185c0d2a9cf4ba8
Abhishek Kumar <sahniak56@gmail.com>
2024-03-09 16:48:42 +0530

    config

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:4500",
+        target: "https://google-passport-auth-server.onrender.com",
       },
     },
   },
